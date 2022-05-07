565b8b697a5e5efef605ceb742e57038634adb9e
adebayoAdesina <adebayoadesina789@gmail.com>
2022-05-07 12:28:34 +0100

    working on the clickable containers

--- lib/State_Management/provider.dart
+++ lib/State_Management/provider.dart
@@ -8,6 +8,7 @@ class AppData with ChangeNotifier {
   String playerOneUsing = '';
   String playerTwoName = '';
   String playerTwoUsing = '';
+  String currentUsing = '';
 
   List totalPlayers = [
 
@@ -30,10 +31,72 @@ class AppData with ChangeNotifier {
         break;
       default:
         playerTwoUsing = value;
+        currentUsing = playerOneUsing;
     }
     notifyListeners();
   }
+  String _containerOne = '';
+  String _containerTwo = '';
+  final String _containerThree = '';
+  final String _containerFour = '';
+  final String _containerFive = '';
+  final String _containerSix = '';
+  final String _containerSeven = '';
+  final String _containerEight = '';
+  final String _containerNine = '';
 
+  void playingGame(String value) async{
+    if (currentUsing == playerOneUsing) {
+      switch (value) {
+        case 'containerOne':
+          if (_containerOne == '') {
+            _containerOne =currentUsing;
+            print(_containerOne);
+            currentUsing = playerTwoUsing;
+          }
+          break;
+        case 'containerTwo':
+          if (_containerTwo == '') {
+            _containerTwo =currentUsing;
+            print(_containerTwo);
+            currentUsing = playerTwoUsing;
+          }
+          break;
+        default:
+      }
+    } else if (currentUsing == playerTwoUsing) {
+      switch (value) {
+        case 'containerOne':
+          if (_containerOne == '') {
+            _containerOne =currentUsing;
+            print(_containerOne);
+            currentUsing = playerOneUsing;
+          }
+          break;
+        case 'containerTwo':
+          if (_containerTwo == '') {
+            _containerTwo =currentUsing;
+            print(_containerTwo);
+            currentUsing = playerOneUsing;
+          }
+          break;
+        default:
+      }
+    }
+    // else {
+    //   currentUsing = playerOneUsing;
+    // }
+    notifyListeners();
+  }
   int get scoreA => _scoreA;
   int get scoreB => _scoreB;
+  String get containerone => _containerOne;
+  String get containerTwo => _containerTwo;
+  String get containerThree => _containerThree;
+  String get containerFour => _containerFour;
+  String get containerFive => _containerFive;
+  String get containerSix => _containerSix;
+  String get containerSeven => _containerSeven;
+  String get containerEight => _containerEight;
+  String get containerNine => _containerNine;
 }
--- lib/Views/game.dart
+++ lib/Views/game.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:tic_tac_toe/Color/colors.dart';
+
+import '../State_Management/provider.dart';
 
 class Game extends StatefulWidget {
   const Game({ Key? key }) : super(key: key);
@@ -14,164 +17,179 @@ var width = 100.0;
 class _GameState extends State<Game> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: textColor,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(
-            top: 20,
-            left: 8,
-            right: 8
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: height,
-                    width: width,
-                    decoration: const BoxDecoration(
-                      color: whiteColor,
-                      border: Border(
-                        right: BorderSide(
-                          width: 2,
-                          color: textColor
-                        ),  
-                      )
-                    ),
-                  ),
-                  Container(
-                    height: height,
-                    width: width,
-                    decoration: const BoxDecoration(
-                      color: whiteColor,
-                    ),
-                  ),
-                  Container(
-                    height: height,
-                    width: width,
-                    decoration: const BoxDecoration(
-                      color: whiteColor,
-                      border: Border(
-                        left: BorderSide(
-                          width: 2,
-                          color: textColor
-                        )
-                      )
-                    ),
-                  )
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: height,
-                    width: width,
-                    decoration: const BoxDecoration(
-                      color: whiteColor,
-                      border: Border(
-                        right: BorderSide(
-                          width: 2,
-                          color: textColor
-                        ),
-                        top: BorderSide(
-                          width: 2,
-                          color: textColor
-                        ),
-                        bottom: BorderSide(
-                          width: 2,
-                          color: textColor
-                        ),
-                      )
-                    ),
-                  ),
-                  Container(
-                    height: height,
-                    width: width,
-                    decoration: const BoxDecoration(
-                      color: whiteColor,
-                      border: Border(
-                        bottom: BorderSide(
-                          width: 2,
-                          color: textColor
-                        ),
-                        top: BorderSide(
-                          width: 2,
-                          color: textColor
+    return ChangeNotifierProvider(
+      create: ((context) => AppData()),
+      builder: (context, child) {
+        return Scaffold(
+        backgroundColor: textColor,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(
+              top: 20,
+              left: 8,
+              right: 8
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    InkWell(
+                      onTap: (() {
+                        context.read<AppData>().playingGame('containerOne');
+                      }),
+                      child: Container(
+                        height: height,
+                        width: width,
+                        decoration: const BoxDecoration(
+                          color: whiteColor,
+                          border: Border(
+                            right: BorderSide(
+                              width: 2,
+                              color: textColor
+                            ),  
+                          )
                         ),
                       ),
                     ),
-                  ),
-                  Container(
-                    height: height,
-                    width: width,
-                    decoration: const BoxDecoration(
-                      color: whiteColor,
-                      border: Border(
-                        left: BorderSide(
-                          width: 2,
-                          color: textColor
+                    InkWell(
+                      onTap: (() {
+                        context.read<AppData>().playingGame('containerTwo');
+                      }),
+                      child: Container(
+                        height: height,
+                        width: width,
+                        decoration: const BoxDecoration(
+                          color: whiteColor,
                         ),
-                        top: BorderSide(
-                          width: 2,
-                          color: textColor
-                        ),
-                        bottom: BorderSide(
-                          width: 2,
-                          color: textColor
-                        ),
-                      )
+                      ),
                     ),
-                  )
-                ],
-              ),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: height,
-                    width: width,
-                    decoration: const BoxDecoration(
-                      color: whiteColor,
-                      border: Border(
-                        right: BorderSide(
-                          width: 2,
-                          color: textColor
-                        ),  
-                      )
-                    ),
-                  ),
-                  Container(
-                    height: height,
-                    width: width,
-                    decoration: const BoxDecoration(
-                      color: whiteColor,
-                      
-                    ),
-                  ),
-                  Container(
-                    height: height,
-                    width: width,
-                    decoration: const BoxDecoration(
-                      color: whiteColor,
-                      border: Border(
-                        left: BorderSide(
-                          width: 2,
-                          color: textColor
+                    Container(
+                      height: height,
+                      width: width,
+                      decoration: const BoxDecoration(
+                        color: whiteColor,
+                        border: Border(
+                          left: BorderSide(
+                            width: 2,
+                            color: textColor
+                          )
                         )
-                      )
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: height,
+                      width: width,
+                      decoration: const BoxDecoration(
+                        color: whiteColor,
+                        border: Border(
+                          right: BorderSide(
+                            width: 2,
+                            color: textColor
+                          ),
+                          top: BorderSide(
+                            width: 2,
+                            color: textColor
+                          ),
+                          bottom: BorderSide(
+                            width: 2,
+                            color: textColor
+                          ),
+                        )
+                      ),
                     ),
-                  )
-                ],
-              )
-            ],
+                    Container(
+                      height: height,
+                      width: width,
+                      decoration: const BoxDecoration(
+                        color: whiteColor,
+                        border: Border(
+                          bottom: BorderSide(
+                            width: 2,
+                            color: textColor
+                          ),
+                          top: BorderSide(
+                            width: 2,
+                            color: textColor
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: height,
+                      width: width,
+                      decoration: const BoxDecoration(
+                        color: whiteColor,
+                        border: Border(
+                          left: BorderSide(
+                            width: 2,
+                            color: textColor
+                          ),
+                          top: BorderSide(
+                            width: 2,
+                            color: textColor
+                          ),
+                          bottom: BorderSide(
+                            width: 2,
+                            color: textColor
+                          ),
+                        )
+                      ),
+                    )
+                  ],
+                ),
+    
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: height,
+                      width: width,
+                      decoration: const BoxDecoration(
+                        color: whiteColor,
+                        border: Border(
+                          right: BorderSide(
+                            width: 2,
+                            color: textColor
+                          ),  
+                        )
+                      ),
+                    ),
+                    Container(
+                      height: height,
+                      width: width,
+                      decoration: const BoxDecoration(
+                        color: whiteColor,
+                        
+                      ),
+                    ),
+                    Container(
+                      height: height,
+                      width: width,
+                      decoration: const BoxDecoration(
+                        color: whiteColor,
+                        border: Border(
+                          left: BorderSide(
+                            width: 2,
+                            color: textColor
+                          )
+                        )
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
           ),
         ),
-      ),
+      );
+      },
     );
   }
 }
